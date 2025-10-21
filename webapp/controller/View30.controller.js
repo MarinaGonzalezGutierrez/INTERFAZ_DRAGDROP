f4aeda55a1b6c726d10dea2cd562aa32d03ce7bf
Marina González Gutiérrez <marina.gonzalez-gutierrez.external@capgemini.com>
2025-10-21 06:44:23 +0000

    rating estrella

--- webapp/controller/View30.controller.js
+++ webapp/controller/View30.controller.js
@@ -2,32 +2,36 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel"
-
 ], function (Controller, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("ejercicio15.project1.controller.View30", {
         onInit: function () {
 
-            var oModel = new sap.ui.model.json.JSONModel("model/products.json");
-            this.getView().setModel(oModel);
+            const oData = {
+                products: [
+                    { id: "P001", name: "Producto A", rating: 0 },
+                    { id: "P002", name: "Producto B", rating: 0 },
+                    { id: "P003", name: "Producto C", rating: 0 }
+                ]
 
+            };
+
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
         },
+
 
 
         onRatingChange: function (oEvent) {
-            var fValue = oEvent.getParameter("value");
-            this.getView().getModel().setProperty("/rating", fValue);
-        },
+            const fNewRating = oEvent.getParameter("value");
+            const oItem = oEvent.getSource().getBindingContext().getObject();
+            oItem.rating = fNewRating;
 
-
-        onSaveRating: function () {
-            var fRating = this.getView().getModel().getProperty("/rating");
-            // Aquí podrías hacer una llamada a un backend o guardar en localStorage
-            MessageToast.show("Calificación guardada: " + fRating);
+            // Simulación de guardado
+            console.log(`Producto ${oItem.name} calificado con ${fNewRating} estrellas`);
+            
         }
-
-
 
     });
 });
